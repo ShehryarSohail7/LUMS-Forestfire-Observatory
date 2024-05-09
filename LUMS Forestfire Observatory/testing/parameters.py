91b5dbcd42e2ba004b61003b29ad11e89e05f5bf
Shehryar Sohail <24100120@lums.edu.pk>
2024-05-09 17:52:44 +0500

    regional data success

--- LUMS Forestfire Observatory/testing/parameters.py
+++
@@ -1,9 +0,0 @@
-import ee 
-import os
-# from io import StringIO
-
-ee.Authenticate()
-service_account = "lums-forest-fire-observatory@forest-fire-obs.iam.gserviceaccount.com"
-credentials = ee.ServiceAccountCredentials(service_account, "forest-fire-obs-5966ba29fefa.json")
-ee.Initialize(credentials=credentials, project='forest-fire-obs')
-print(ee.String('Hello, Earth Engine!').getInfo())
